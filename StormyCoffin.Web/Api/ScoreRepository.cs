a644a7c3295cd1b0ff482cd9bcfb202cff59cfc3
Anders Kofoed <anders.kfd@gmail.com>
2017-02-27 17:04:56 +0100

    Add initial scores

--- StormyCoffin.Web/Api/ScoreRepository.cs
+++ StormyCoffin.Web/Api/ScoreRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,11 @@ namespace StormyCoffin.Web.Api
 {
     public static class ScoreRepository
     {
-        public static Dictionary<int, List<string>> UserScores { get; set; } = new Dictionary<int, List<string>>();
+        public static Dictionary<int, List<string>> UserScores { get; set; } = new Dictionary<int, List<string>>()
+        {
+            {1, new List<string> { "45", "67" } },
+            {4, new List<string> { "10", "26" } },
+            {9, new List<string> { "90", "10" } },
+        };
     }
 }
